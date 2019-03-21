652d95fdc59b2b012dd5e03a886e8f2eb0df1bab
Vladimir <kozyrev-vova@mail.ru>
2019-03-21 08:10:06 +0300

    Добавлены тесты на HTMLEncode

---
+++ lab_2/WorkVector/HTMLEncode_Test/HTMLEncode_Test.cpp
@@ -0,0 +1,18 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "lab_2\WorkVector\HTMLEncode\Encode_HTML_String.h"
+
+TEST_CASE("Check HtmlEncode()")
+{
+	{
+		std::string htmlPart = "<div>PutIn</div>";
+		std::string encodeHtmlString = "&lt;div&gt;PutIn&lt;/div&gt;";
+		REQUIRE(HtmlEncode(htmlPart) == encodeHtmlString);
+	}
+
+	{
+		std::string htmlPart = "<p\"> asd;&sdjfn </p>";
+		std::string encodeHtmlString = "&lt;p&quot;&gt; asd;&amp;sdjfn &lt;/p&gt;";
+		REQUIRE(HtmlEncode(htmlPart) == encodeHtmlString);
+	}
+};

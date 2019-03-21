e9e6c3edf8e5a76d218fae57d8a38d2d35939cdb
Vladimir <kozyrev-vova@mail.ru>
2019-03-21 09:11:38 +0300

    правки в тестах для vector

--- lab_2/WorkVector/WorkVector_Test/WorkVector_Test.cpp
+++ lab_2/WorkVector/WorkVector_Test/WorkVector_Test.cpp
@@ -2,19 +2,35 @@
 #include "catch.hpp"
 #include "lab_2\WorkVector\WorkVector\ChangeVector.h"
 
-TEST_CASE("Chech WorkWithVector()")
+TEST_CASE("Check WorkWithVector()")
 {
 	{
 		std::vector<double> numbers = { 1, 2, 3, 6, 4 };
-		std::vector<double> result = WorkWithVector(numbers);
-		std::vector<double> compare = { 1, 2, 3, 4, 6 };
-		REQUIRE(result == compare);
+		WorkWithVector(numbers);
+		SortVector(numbers);
+		std::vector<double> result = { 1, 2, 3, 4, 6 };
+		REQUIRE(numbers == result);
 	}
+}
 
+TEST_CASE("Negative WorkWithVector()")
+{
 	{
 		std::vector<double> numbers = { 1, -2, 3, 6, 4 };
-		std::vector<double> result = WorkWithVector(numbers);
+		WorkWithVector(numbers);
+		SortVector(numbers);
 		std::vector<double> compare = { -12, -8, -6, -2, 4 };
-		REQUIRE(result == compare);
+		REQUIRE(numbers == compare);
+	}
+}
+
+TEST_CASE("Empty WorkWithVector()")
+{
+	{
+		std::vector<double> numbers = {};
+		WorkWithVector(numbers);
+		SortVector(numbers);
+		std::vector<double> compare = {};
+		REQUIRE(numbers == compare);
 	}
 }

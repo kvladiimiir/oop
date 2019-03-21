e10900e8d3218ed7116859656f11185469530789
Vladimir <kozyrev-vova@mail.ru>
2019-03-21 08:09:00 +0300

    Добавлены тесты на Решето Эратосфена

---
+++ lab_2/WorkVector/GeneratePrimeNumbers_Test/GeneratePrimeNumbers_Test.cpp
@@ -0,0 +1,23 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <set>
+#include "lab_2\WorkVector\GeneratePrimeNumbers\SieveEratosfen.h"
+
+TEST_CASE("Check size setPrimeNumbers")
+{
+	REQUIRE(GeneratePrimeNumbersSet(0).size() == 0);
+	REQUIRE(GeneratePrimeNumbersSet(2).size() == 1);
+	REQUIRE(GeneratePrimeNumbersSet(10).size() == 4);
+};
+
+TEST_CASE("Check setPrimeNumbers")
+{
+	REQUIRE(GeneratePrimeNumbersSet(0) == std::set<int> {});
+	REQUIRE(GeneratePrimeNumbersSet(2) == std::set<int> { 2 });
+	REQUIRE(GeneratePrimeNumbersSet(10) == std::set<int> { 2, 3, 5, 7 });
+};
+
+TEST_CASE("Check setPrimeNumbers at 100000000")
+{
+	REQUIRE(GeneratePrimeNumbersSet(100000000).size() == 5761455);
+};

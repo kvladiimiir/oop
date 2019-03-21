666e5e52f5a762ebd01eae55bead25e5af7cb2ee
Vladimir <kozyrev-vova@mail.ru>
2019-03-21 08:11:42 +0300

    Добавлены тесты для 2 лр 1 задания работы с вектором

---
+++ lab_2/WorkVector/WorkVector_Test/WorkVector_Test.cpp
@@ -0,0 +1,20 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "lab_2\WorkVector\WorkVector\ChangeVector.h"
+
+TEST_CASE("Chech WorkWithVector()")
+{
+	{
+		std::vector<double> numbers = { 1, 2, 3, 6, 4 };
+		std::vector<double> result = WorkWithVector(numbers);
+		std::vector<double> compare = { 1, 2, 3, 4, 6 };
+		REQUIRE(result == compare);
+	}
+
+	{
+		std::vector<double> numbers = { 1, -2, 3, 6, 4 };
+		std::vector<double> result = WorkWithVector(numbers);
+		std::vector<double> compare = { -12, -8, -6, -2, 4 };
+		REQUIRE(result == compare);
+	}
+}
